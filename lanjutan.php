eb653928ba835889e59c3575798f00cd4af735ac
Pangki Irawan <pangki1818@gmail.com>
2022-12-24 16:01:06 +0700

    lanjutan

---
+++ lanjutan.php
@@ -0,0 +1,52 @@
+<?php
+$nama = "Pangki"; 
+/*
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+*/
+
+/*
+$no = 10;
+for($i=0; $i<$no; $i++) {
+    $n = $i + 1; 
+    echo $n. " "<br/>";
+}
+*/
+
+/*
+$no = 10;
+$i = 0;
+
+while ($i < $no) 
+{$n = $i + 1;
+    echo $n."$nama."<br/>";
+    $i++;
+}
+*/
+
+/*
+$no = 10;
+$i = 0;
+do {
+    $n = $i + 1;
+    echo $n."$nama."<br/>";
+    $i++;
+} while ($i < $no)
+*/
+
+$data = array('avanza', 'lamborgini', 'tesla', 'xenia', 'xpander', 'rubicon');
+
+echo $data[5];
+/*
+foreach($data as $value) {
+    echo $value.<br>;
+}
+*/s
+?>
